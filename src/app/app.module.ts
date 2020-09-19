0a6e3bc2a850f29299357bc1e682f3b00900f04b
Marcus Britto <marcus.britto@gmail.com>
2020-09-19 12:16:22 -0300

    Aula 79

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { JogoDaVelhaModule } from './jogo-da-velha';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    JogoDaVelhaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
